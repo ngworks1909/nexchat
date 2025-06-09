57893a837b7ec31616d9b5d25e2c081d2870c60c
ngworks1909 <nithin.kanduru1908@gmail.com>
2025-06-09 12:14:57 +0530

    change

--- apps/whatsapp/firebase/firebase.ts
+++ apps/whatsapp/firebase/firebase.ts
@@ -1,28 +1,21 @@
 import { initializeApp } from "firebase/app";
 import {getStorage} from 'firebase/storage';
 import {getAuth} from 'firebase/auth'
+import dotenv from 'dotenv'
 
+dotenv.config()
 
-// const firebaseConfig = {
-//   apiKey: `${'AIzaSyCaKjG8FJsvbN30ZlvDSJi23qQbsyi6LHY'}`,
-//   authDomain: `${'whatsapp-f7e8b.firebaseapp.com'}`,
-//   projectId: `${'whatsapp-f7e8b'}`,
-//   storageBucket: `whatsapp-f7e8b.appspot.com`,
-//   messagingSenderId: `${process.env.FIREBASE_MESSAGE_ID}`,
-//   appId: `${process.env.FIREBASE_APP_ID}`,
-//   measurementId: `${process.env.FIREBASE_MEASUREMENT_ID}`
-// };
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCaKjG8FJsvbN30ZlvDSJi23qQbsyi6LHY",
-    authDomain: "whatsapp-f7e8b.firebaseapp.com",
-    databaseURL: "https://whatsapp-f7e8b-default-rtdb.firebaseio.com",
-    projectId: "whatsapp-f7e8b",
-    storageBucket: "whatsapp-f7e8b.appspot.com",
-    messagingSenderId: "414801914637",
-    appId: "1:414801914637:web:e982ad20d5a501a6e79e44",
-    measurementId: "G-FRP6YB4FS4"
-  };
+  apiKey: process.env.FIREBASE_API_KEY!,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_ID!,
+  appId: process.env.FIREBASE_APP_ID!,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID!
+};
+
 
 
 
